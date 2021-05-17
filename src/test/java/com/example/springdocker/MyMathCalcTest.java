c2dd4698fa1d8bd1a54b6a829de0b36f72f26d7e
Elliot Åberg Fält <elliot.falt@gmail.com>
2021-05-17 09:47:03 +0200

    Tests

---
+++ src/test/java/com/example/springdocker/MyMathCalcTest.java
@@ -0,0 +1,26 @@
+package com.example.springdocker;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MyMathCalcTest {
+    private MyMathCalc mathCalc = new MyMathCalc();
+    int a = 5;
+    int b = 2;
+    int o = 0;
+
+
+    @Test
+    void addTest() {
+        assertEquals(mathCalc.add(a, b), 7);
+    }
+    @Test
+    void multiplyTest() {
+        assertEquals(mathCalc.multiply(a, b), 10);
+    }
+    @Test
+    void divideTest() {
+        assertEquals(mathCalc.divide(a, b), 2.5);
+    }
+}
