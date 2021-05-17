92f21b0369b20fbfc95a32bb6a68ab328f48a4cd
Elliot Åberg Fält <elliot.falt@gmail.com>
2021-05-17 09:27:16 +0200

    divide returnerar nu en double

--- src/main/java/com/example/springdocker/MyMathCalc.java
+++ src/main/java/com/example/springdocker/MyMathCalc.java
@@ -7,7 +7,7 @@ public class MyMathCalc {
     public int multiply(int a, int b) {
         return a*b;
     }
-    public int divide(int a, int b) {
+    public double divide(double a, double b) {
         return a/b;
     }
 }
